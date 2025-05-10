ae0a62538e3760fff62d58cea26ff2fb39e633ea
Breno <brenomelogabriel2019@gmail.com>
2025-05-10 09:19:52 -0300

    refactor: moved business logic from auth controller to auth service for better separation of concerns

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -5,39 +5,17 @@ import jwt from "jsonwebtoken";
 import db from "../db";
 import { usersTable } from "../db/schema";
 import config from "../config/jwt";
+import { validateUserRegister, validateUserLogin } from "../services/AuthService";
 
 async function register(req: Request, res: Response) {
   try {
     const { name, email, password } = req.body;
 
-    if (!name || !email || !password) {
-      res.status(400).json({ message: "Missing required fields" });
-      return;
-    }
+    const result = await validateUserRegister(name, email, password);
 
-    const user = await db
-      .select()
-      .from(usersTable)
-      .where(eq(usersTable.email, email));
+    res.status(result.code).json({ message: result.message });
+    return;
 
-    if (user.length > 0) {
-      res.status(400).json({ message: "User already exists" });
-      return;
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    await db
-      .insert(usersTable)
-      .values({ name, email, password: hashedPassword });
-
-    res.status(201).json({
-      message: "User created successfully",
-      user: {
-        name,
-        email,
-      },
-    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
@@ -48,27 +26,11 @@ async function login(req: Request, res: Response) {
   try {
     const { email, password } = req.body;
 
-    const user = await db
-      .select()
-      .from(usersTable)
-      .where(eq(usersTable.email, email));
+    const result = await validateUserLogin(email, password);
 
+    res.status(result.code).json({ message: result.message, token: result.token });
+    return;
 
-    if (user.length == 0 || !(await bcrypt.compare(password, user[0].password))) {
-      res.status(400).json({ message: "wrong email or password" });
-      return;
-    }
-
-    const token = jwt.sign(
-      { id: user[0].id, name: user[0].name, email: user[0].email },
-      config.secret,
-      { expiresIn: config.expiresIn }
-    );
-
-    res.status(200).json({
-      message: "authentication successful",
-      token: token,
-    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Internal server error" });
---
+++ server/src/services/AuthService.ts
@@ -0,0 +1,62 @@
+import bcrypt from "bcrypt";
+import { eq } from "drizzle-orm";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import db from "../db";
+import { usersTable } from "../db/schema";
+import config from "../config/jwt";
+
+async function validateUserRegister(name: string, email: string, password: string) {
+
+    if (!name || !email || !password) {
+        throw {code: 400, message: "Missing required fields"};
+      }
+  
+      const user = await db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.email, email));
+  
+      if (user.length > 0) {
+        throw {code: 400, message: "User already exists"};
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+    await db
+        .insert(usersTable)
+        .values({ name, email, password: hashedPassword });
+
+    return {
+        code: 201,
+        message: "User created successfully"
+    };
+
+}
+
+async function validateUserLogin(email: string, password: string) {
+
+    const user = await db
+    .select()
+    .from(usersTable)
+    .where(eq(usersTable.email, email));
+
+  if (user.length == 0 || !(await bcrypt.compare(password, user[0].password))) {
+    throw {code: 400, message: "wrong email or password"};
+  }
+
+  const token = jwt.sign(
+    { id: user[0].id, name: user[0].name, email: user[0].email },
+    config.secret,
+    { expiresIn: config.expiresIn }
+  );
+
+    return {
+        code: 200,
+        message: "User logged in successfully",
+        token: token
+    };
+    
+}
+
+export { validateUserRegister, validateUserLogin };
