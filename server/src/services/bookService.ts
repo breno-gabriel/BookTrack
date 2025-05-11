c9a6be485b62b52e0d00d370e0be08765e32dbd2
Breno <brenomelogabriel2019@gmail.com>
2025-05-11 11:40:29 -0300

    feat: prevent updates to books with status 'Lido'

--- server/src/services/bookService.ts
+++ server/src/services/bookService.ts
@@ -81,6 +81,10 @@ async function updateBookService(
     };
   }
 
+  if (status == BookStatus.LIDO) {
+    return { status: 400, message: "Não é possível atualizar um livro lido" };
+  }
+
   const result = bookSchema.safeParse({
     title,
     author,
