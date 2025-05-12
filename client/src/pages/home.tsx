b3dede4b35f7a125ba41a7ba2a8ddfb3875c872e
Breno <brenomelogabriel2019@gmail.com>
2025-05-12 15:51:11 -0300

    feat: render book cards from GET /books API

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,12 +1,33 @@
 import AddBookDialog from "@/components/add-book-dialog";
 import BookCard from "@/components/book-card";
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { Plus } from "lucide-react";
 import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+interface Book {
+  id: string;
+  title: string;
+  author: string;
+  status: string;
+  rating: string;
+}
 
 export default function Home() {
-
   const [open, setOpen] = useState(false);
+
+  const { data, isPending } = useQuery({
+    queryKey: ["books"],
+    queryFn: async () => {
+      const response = await axios.get("http://localhost:3000/books", {
+        headers: {
+          Authorization: localStorage.getItem("token")
+        }
+      });
+      return response.data['books'];
+    },
+  });
 
   return (
     <div className="container mx-auto py-8 px-4">
@@ -17,18 +38,29 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="flex justify-end mb-6">
+      <div className="flex justify-end mb-12">
         <Button className="p-6 cursor-pointer" onClick={() => setOpen(true)}>
           <Plus className="mr-2 h-4 w-4" />
           Adicionar livro
         </Button>
-        <AddBookDialog open={open} onOpenChange={setOpen}/>
+        <AddBookDialog open={open} onOpenChange={setOpen} />
       </div>
-
+      
       <div className="flex flex-wrap gap-4">
-        <BookCard title="Dom Casmurro" author="Machado de Assis" status="Lido" rating="5"/>
+        {isPending ? (
+          <p>Carregando livros...</p>
+        ) : (
+          data?.map((book: Book) => (
+            <BookCard
+              key={book.id}
+              title={book.title}
+              author={book.author}
+              status={book.status}
+              rating={book.rating}
+            />
+          ))
+        )}
       </div>
-
     </div>
   );
 }
