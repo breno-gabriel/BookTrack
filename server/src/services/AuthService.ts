4d958cdcb50b39a4714ebcea38686d44ee5ef66d
Breno <brenomelogabriel2019@gmail.com>
2025-05-13 14:11:37 -0300

    feat: improve data validation with zod

--- server/src/services/AuthService.ts
+++ server/src/services/AuthService.ts
@@ -2,37 +2,53 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import config from "../config/jwt";
 import { getUserByEmail, insertUser } from "../repositories/authRepository";
-import { CreateUserDTO, LoginUserDTO } from "../interfaces/user";
+import z from "zod";
+
+const createUserSchema = z.object({
+  name: z.string().min(3, "Name must have at least 3 characters"),
+  email: z.string().email("Digite um email válido"),
+  password: z.string().min(8, "Password must have at least 8 characters"),
+});
+
+const loginUserSchema = z.object({
+  email: z.string().email("Digite um email válido"),
+  password: z.string().min(8, "Password must have at least 8 characters"),
+});
+
+type CreateUserDTO = z.infer<typeof createUserSchema>;
+type LoginUserDTO = z.infer<typeof loginUserSchema>;
 
 async function validateUserRegister({ name, email, password }: CreateUserDTO) {
 
-    if (!name || !email || !password) {
-        throw {code: 400, message: "Missing required fields"};
-      }
-  
-      const user = await getUserByEmail(email);
-  
-      if (user.length > 0) {
-        throw {code: 400, message: "User already exists"};
-      }
-  
-      const hashedPassword = await bcrypt.hash(password, 10);
-  
-    await insertUser(name, email, hashedPassword);
+  const result = createUserSchema.safeParse({ name, email, password });
 
-    return {
-        code: 201,
-        message: "User created successfully"
-    };
+  if (!result.success) {
+    return { code: 400, message: result.error.format() };
+  }
 
+  const user = await getUserByEmail(email);
+
+  if (user.length > 0) {
+    return { code: 400, message: "User already exists" };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await insertUser(name, email, hashedPassword);
+
+  return {
+    code: 201,
+    message: "User created successfully",
+  };
 }
 
 async function validateUserLogin({ email, password }: LoginUserDTO) {
+  const user = await getUserByEmail(email);
 
-    const user = await getUserByEmail(email);
+  loginUserSchema.parse({ email, password });
 
   if (user.length == 0 || !(await bcrypt.compare(password, user[0].password))) {
-    throw {code: 400, message: "wrong email or password"};
+    return { code: 400, message: "wrong email or password" };
   }
 
   const token = jwt.sign(
@@ -41,12 +57,11 @@ async function validateUserLogin({ email, password }: LoginUserDTO) {
     { expiresIn: config.expiresIn }
   );
 
-    return {
-        code: 200,
-        message: "User logged in successfully",
-        token: token
-    };
-    
+  return {
+    code: 200,
+    message: "User logged in successfully",
+    token: token,
+  };
 }
 
 export { validateUserLogin, validateUserRegister };
