2ff5fdd0eaf588d15fa43e448d331977a3df5c0b
Breno <brenomelogabriel2019@gmail.com>
2025-05-13 01:02:44 -0300

    feat: improve error handling

--- server/src/controllers/bookController.ts
+++ server/src/controllers/bookController.ts
@@ -10,57 +10,87 @@ import {
 } from "../services/bookService";
 
 async function createBookController(req: Request, res: Response) {
-  const { title, author, status, avaliation }: createBook = req.body;
-  const user_id = req.user.id;
+  try {
+    const { title, author, status, avaliation }: createBook = req.body;
+    const user_id = req.user.id;
 
-  const result = await createBookService(
-    { title, author, status, avaliation },
-    user_id
-  );
+    const result = await createBookService(
+      { title, author, status, avaliation },
+      user_id
+    );
 
-  res.status(result.status).json({ message: result.message });
-  return;
+    res.status(result.status).json({ message: result.message });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 async function getBooksController(req: Request, res: Response) {
-  const books = await getBooksService();
-  res.json({ message: "Livros encontrados", books });
-  return;
+  try {
+    const books = await getBooksService();
+    res.json({ message: "Livros encontrados", books });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 async function getBookByIdController(req: Request, res: Response) {
-  const book = await getBookByIdService(req.params.id);
-  res.json({ message: "Livro encontrado", book });
-  return;
+  try {
+    const book = await getBookByIdService(req.params.id);
+    res.json({ message: "Livro encontrado", book });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 async function deleteBookController(req: Request, res: Response) {
-  const result = await deleteBookService(req.params.id, req.user.id);
-  res.status(result.status).json({ message: result.message });
-  return;
+  try {
+    const result = await deleteBookService(req.params.id, req.user.id);
+    res.status(result.status).json({ message: result.message });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 async function updateBookController(req: Request, res: Response) {
-  const { title, author, status, avaliation }: updateBook = req.body;
-  const user_id = req.user.id;
-  const result = await updateBookService(
-    req.params.id,
-    {
-      title,
-      author,
-      status,
-      avaliation,
-    },
-    user_id
-  );
-  res.status(result.status).json({ message: result.message });
-  return;
+  try {
+    const { title, author, status, avaliation }: updateBook = req.body;
+    const user_id = req.user.id;
+    const result = await updateBookService(
+      req.params.id,
+      {
+        title,
+        author,
+        status,
+        avaliation,
+      },
+      user_id
+    );
+    res.status(result.status).json({ message: result.message });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 async function getBooksByUserIdController(req: Request, res: Response) {
-  const books = await getBooksByUserIdService(req.params.id);
-  res.json({ message: "Livros encontrados", books });
-  return;
+  try {
+    const books = await getBooksByUserIdService(req.params.id);
+    res.json({ message: "Livros encontrados", books });
+    return;
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
 
 export {
